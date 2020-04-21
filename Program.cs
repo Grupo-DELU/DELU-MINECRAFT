5e1d0720fb731c71f844a434feed4ef34847da43
Christian Oliveros <christianol_01@hotmail.com>
2020-04-20 21:42:25 -0400

    Loading Blocks from MCEdit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@
 //using Keras;
 using System.IO;
 using Delu_Mc.Pipes;
+using Delu_Mc.MCEdit;
+using Delu_Mc.MCEdit.Block;
+
 
 namespace Delu_Mc
 {
@@ -12,15 +15,6 @@ namespace Delu_Mc
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //var hello = tf.constant("Hello, TensorFlow!");
-
-            // Start tf session
-            //using (var sess = tf.Session())
-            //{
-            //    // Run the op
-            //    var result = sess.run(hello);
-            //    Console.WriteLine($"{result}");
-            //}
             Console.WriteLine(MCEdit.Block.ClassicMaterials.Stone_1_0);
 
             if (args.Length != 1)
@@ -35,17 +29,47 @@ namespace Delu_Mc
             {
                 BinaryWriter write = new BinaryWriter(new MemoryStream());
 
-                int arrSize = reader.ReadInt32();
-                Console.WriteLine($"Size: {arrSize}");
-                write.Write(arrSize);
+                int ySize = reader.ReadInt32();
+                int zSize = reader.ReadInt32();
+                int xSize = reader.ReadInt32();
 
-                for (int i = 0; i < arrSize; i++)
+                Console.WriteLine($"Y: {ySize} Z: {zSize} X: {xSize}");
+                Material[][][] blocks = new Material[ySize][][];
+                for (int y = 0; y < ySize; y++)
                 {
-                    int num = reader.ReadInt32();
-                    Console.WriteLine(num);
-                    write.Write(num);
+                    blocks[y] = new Material[zSize][];
+                    for (int z = 0; z < zSize; z++)
+                    {
+                        blocks[y][z] = new Material[xSize];
+                        for (int x = 0; x < xSize; x++)
+                        {
+                            blocks[y][z][x] = AlphaMaterials.Set.GetMaterial(reader.ReadInt32(), reader.ReadInt32());
+                        }
+                    }
                 }
 
+                // Do stuff here
+
+                for (int y = 0; y < ySize; y++)
+                {
+                    for (int z = 0; z < zSize; z++)
+                    {
+                        for (int x = 0; x < xSize; x++)
+                        {
+                            // TODO: Remove this test
+                            if (blocks[y][z][x] == AlphaMaterials.Air_0_0)
+                            {
+                                write.Write(AlphaMaterials.Stone_1_0.ID);
+                                write.Write(AlphaMaterials.Stone_1_0.Data);
+                            }
+                            else
+                            {
+                                write.Write(blocks[y][z][x].ID);
+                                write.Write(blocks[y][z][x].Data);
+                            }
+                        }
+                    }
+                }
                 pipeClient.WriteMemoryBlock((MemoryStream)write.BaseStream);
             }
             pipeClient.DeInit();
--- Python/deluMc.py
+++ Python/deluMc.py
@@ -11,19 +11,39 @@ def perform(level, box, options):
     pipeProcess = pipeProcessHandler.PipeProcess()
     pipeProcess.open()
     pipeServer = pipeProcess.getPipeServer()
-    import random
+
+    # box.max is open set
+    x_size = box.maxx - box.minx
+    y_size = box.maxy - box.miny
+    z_size = box.maxz - box.minz
 
     writer = pipeHandler.MemoryStreamWriter()
-    arrSize = 10
-    writer.writeInt32(arrSize)
-    for _ in xrange(arrSize):
-        writer.writeInt32(random.randint(-10, 10))
 
+    # We are going to access using YZX for better cache access
+    writer.writeInt32(y_size)
+    writer.writeInt32(z_size)
+    writer.writeInt32(x_size)
+
+    for y in xrange(box.miny, box.maxy):
+        for z in xrange(box.minz, box.maxz):
+            for x in xrange(box.minx, box.maxx):
+                block_id = level.blockAt(x, y, z)
+                block_data = level.blockDataAt(x, y, z)
+                writer.writeInt32(block_id)
+                writer.writeInt32(block_data)
+
+    # Send Work
     pipeServer.writeMemoryBlock(writer.getRawBuffer())
+
+    # Receive Work
     reader = pipeServer.readMemoryBlock()
-    readSize = reader.readInt32()
-    print("Size: " + str(readSize))
-    for _ in xrange(readSize):
-        print(reader.readInt32())
+    # We assume that we are receiving it the same way we sent it
+    for y in xrange(box.miny, box.maxy):
+        for z in xrange(box.minz, box.maxz):
+            for x in xrange(box.minx, box.maxx):
+                block_id = reader.readInt32()
+                block_data = reader.readInt32()
+                level.setBlockAt(x, y, z, block_id)
+                level.setBlockDataAt(x, y, z, block_data)
 
     pipeProcess.close()
