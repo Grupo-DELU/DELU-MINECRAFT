cf0ed87a5a61d6cfa4bfb2211824b94387e3c9ca
Christian Oliveros <christianol_01@hotmail.com>
2020-04-24 21:58:06 -0400

    Linux publish script

---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo Publishing Linux x64
+dotnet publish --configuration Release -p:Platform=x64 -p:OS=Unix -r linux-x64
+echo Publishing Windows x64
+dotnet publish --configuration Release -p:Platform=x64 -p:OS=Windows_NT -r win-x64
+echo Publishing Windows x32
+dotnet publish --configuration Release -p:Platform=x86 -p:OS=Windows_NT -r win-x86
