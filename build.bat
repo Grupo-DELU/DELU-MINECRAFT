0518242a31633bbb1892757807ae04f10cd44ae7
Christian Oliveros <christianol_01@hotmail.com>
2020-04-24 22:34:52 -0400

    Build Script for Windows

--- build.bat
+++ build.bat
@@ -11,3 +11,10 @@ echo Building Windows x64 %build_type%
 dotnet build --configuration %build_type% -p:Platform=x64 -p:OS=Windows_NT -r win-x64
 echo Building Windows x32 %build_type%
 dotnet build --configuration %build_type% -p:Platform=x86 -p:OS=Windows_NT -r win-x86
+
+Rem This magic comes from: https://superuser.com/a/1413170
+for /f "delims=" %%a in ('powershell.exe -command "& {write-host $([Environment]::GetFolderPath('MyDocuments'))}"') do Set "$documents_path=%%a"
+Rem Echo Value received from Powershell : %$documents_path%\MCEdit\Filters
+Rem For older Machines Maybe reg query "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Personal
+echo Copying Files from ".\Python" to "%$documents_path%\MCEdit\Filters"
+xcopy /y /s /Q /i ".\Python" "%$documents_path%\MCEdit\Filters"
